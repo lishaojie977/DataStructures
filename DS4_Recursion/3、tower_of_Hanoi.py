3f0584d34abd2131a7284d4cb2c32ac31d7cd577
lishaojie977 <1171987472@qq.com>
2020-05-12 17:50:23 +0800

    '迷宫'

--- DS4_Recursion/3、tower_of_Hanoi.py
+++ DS4_Recursion/3、tower_of_Hanoi.py
@@ -52,10 +52,65 @@ move(5,'A','B','C')
     2. 方格被访问过，未避免陷入循环不在此位置继续寻找
     3. 碰到边缘，表示成功
     4. 四个方向探索失败，游戏失败
+
+    turtle  
+    __init__  用来读取迷宫的数据，初始化迷宫内部，并找到游戏精灵的初始位置
+    draw_maze  用来在屏幕上绘制迷宫
+    update_position   用来更新迷宫内的状态和游戏精灵的位置
+    is_exit  用来判断当前位置是否是出口
 '''
+# 迷宫的地图：从txt文件中获取,地图是一个只包含： +  空格的文件 
+
+# 用到的绘制迷宫的符号：+  空格
+# turtle   GUI库绘制迷宫地图
+
+from turtle import Turtle
+class Maze():
+    def __init__(self,mazeFileName):
+        rowsInMaze = 0
+        columnsInMaze = 0
+        self.mazelist = []
+
+        mazeFile = open(mazeFileName,'r')
+        
+        for line in mazeFile:
+            rowList = []
+            col = 0
+            for ch in line[:-1]:
+                rowList.append(ch)
+                if ch == 'Q':
+                    self.startRow = rowsInMaze
+                    self.starCol = col
+                col = col + 1
+                rowsInMaze = rowsInMaze + 1
+                self.mazelist.append(rowList)
+                columnsInMaze = len(rowList)
+
+            self.rowsInMaze = rowsInMaze
+            self.columnsInMaze = columnsInMaze
+            self.xTranslate = columnsInMaze
+            self.yTranslate = rowsInMaze
+            self.t = Turtle()
+            setup(width=600,height=600)
+            setwordcoordinates(-(columnsInMaze-1)/2-.5,
+                               -(rowsInMaze-1)/2-.5,
+                               (columnsInMaze-1)/2+.5,
+                               (rowsInMaze-1)/2+.5)
+    # 绘制屏幕
+    def draw_maze():
+        tracer(0)
+        self.t.up()
+        self.t.goto(x-.5,y-.5)
+        self.t.color('black',color)
+        self.t.heading(90)
+        self.t.down
+        self.t.begin_fill()
+        for i in range(4):
+            self.t.f
 
 
-
+mazeFileName = 'DS4_Recursion/maze.txt'
+maze = Maze(mazeFileName)
 
 
 
